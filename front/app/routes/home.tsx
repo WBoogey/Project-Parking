67cdb47309d7723f56e6e85532a4c7d25c0a1553
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-27 16:44:50 +0100

    emptyied front

--- front/app/routes/home.tsx
+++ front/app/routes/home.tsx
@@ -1,13 +1,12 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: "Project Parking" },
+    { name: "description", content: "Project Parking" },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Home</div>
 }
