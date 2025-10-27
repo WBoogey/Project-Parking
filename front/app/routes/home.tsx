d6f072cee3e89e4db1aaa59a1cf0f9e0f4ebb5ff
Arthur Jenck <arthurjenckdev@gmail.com>
2025-10-27 16:48:37 +0100

    improv: added prettier

--- front/app/routes/home.tsx
+++ front/app/routes/home.tsx
@@ -1,12 +1,12 @@
-import type { Route } from "./+types/home"
+import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Project Parking" },
     { name: "description", content: "Project Parking" },
-  ]
+  ];
 }
 
 export default function Home() {
-  return <div>Home</div>
+  return <div>Home</div>;
 }
