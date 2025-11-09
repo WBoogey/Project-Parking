ab929616a8d4ab1e296101f39d188ee6e6e53fdc
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-11-09 10:46:42 +0100

    add owner repository interface

---
+++ back/src/Domain/Owner/OwnerRepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Domain\Owner;
+
+use App\Domain\Owner\Owner;
+
+interface OwnerRepositoryInterface
+{
+    /**
+     * Sauvegarder un propriétaire (création ou mise à jour)
+     */
+    public function save(Owner $owner): void;
+
+    /**
+     * Récupérer un propriétaire par son ID, email ou nom
+     */
+    public function findById(int $id): ?Owner;
+    public function findByEmail(string $email): ?Owner;
+    public function findByFirstName(string $firstName): ?Owner;
+    public function findByLastName(string $lastName): ?Owner;
+
+    /**
+     * Supprimer un propriétaire du domaine
+     */
+    public function delete(Owner $owner): void;
+
+    /**
+     * Liste des parkings d'un propriétaire
+     */
+    public function getParkings(Owner $owner): array;
+}
