519cda2498dc49ad9042abcc432ad369f2978680
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-11-12 11:41:02 +0100

    config getters

--- back/src/Domain/Parking/ParkingRepositoryInterface.php
+++ back/src/Domain/Parking/ParkingRepositoryInterface.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Parking;
 
+use App\Domain\Owner\Owner;
 use App\Domain\Parking\Parking;
 
 interface ParkingRepositoryInterface
@@ -9,10 +10,12 @@ interface ParkingRepositoryInterface
     public function save(Parking $parking): void;
 
     /**
-     * Récupérer un parking par son ID sa location
+     * Récupérer un parking par son ID, location et owner
+     * 
      */
     public function findById(int $id): ?Parking;
     public function findByLocation(string $location): ?Parking;
 
+    public function findByOwner(Owner $owner): ?array;
     public function delete(Parking $parking): void;
 }
--- back/src/Domain/User/User.php
+++ back/src/Domain/User/User.php
@@ -6,16 +6,25 @@ namespace App\Domain\User;
 
 class User
 {
+    private int $id;
     private string $email;
     private string $password;
     private string $firstName;
     private string $lastName;
     
-    public function __construct(string $email, string $password, string $firstName, string $lastName)
+    public function __construct(int $id, string $email, string $password, string $firstName, string $lastName)
     {
+        $this->id = $id;
         $this->email = $email;
         $this->password = $password;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
     }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    
 }
