5704e025a116a18c1e01434c5b7936ba974c990f
Arthur Jenck <arthurjenckdev@gmail.com>
2025-11-28 15:57:48 +0100

    feat: activated eslinter

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -1,14 +1,39 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
     languageOptions: { globals: globals.browser },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-unused-expressions": "warn",
+    },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
   },
   pluginReact.configs.flat.recommended,
-]);
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+    },
+  },
+];
