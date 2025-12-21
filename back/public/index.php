eadd58070f91512ba6d0e54b3bd140519d58f72c
Arthur Jenck <arthurjenckdev@gmail.com>
2025-12-21 15:39:15 +0100

    feat: docker configuration / cors error

--- back/public/index.php
+++ back/public/index.php
@@ -18,7 +18,19 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 function cors(): void
 {
-  header("Access-Control-Allow-Origin: *");
+  $allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:5173",
+  ];
+
+  $origin = $_SERVER["HTTP_ORIGIN"] ?? "";
+
+  if (in_array($origin, $allowedOrigins)) {
+    header("Access-Control-Allow-Origin: " . $origin);
+    header("Access-Control-Allow-Credentials: true");
+  }
+
   header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
   header("Content-Type: application/json; charset=UTF-8");
   header(
